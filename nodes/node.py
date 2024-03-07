7df445d82831b440e5522a8de03bc39b0af3413e
kirbyfern <kirbyjames.fernandez01@student.csulb.edu>
2024-03-06 18:10:43 -0800

    Modified node.py to receive message from more nodes from master

--- nodes/node.py
+++ nodes/node.py
@@ -1,28 +1,12 @@
-# import socket
-# import logging
-
-# logging.basicConfig(level=logging.INFO)
-
-# def receive_messages():
-#     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-#         s.bind(('', 12345))
-#         while True:
-#             data, addr = s.recvfrom(1024)
-#             message = data.decode()
-#             logging.info("node has received message: %s from %s", message, addr)
-
-# if __name__ == "__main__":
-#     receive_messages()
-
 import socket
 import logging
 import argparse
 
 logging.basicConfig(level=logging.INFO)
 
-def receive_messages(node_id, ip, port):
+def receive_messages(node_id):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((ip, port))
+        s.bind(('', 12345))
         while True:
             data, addr = s.recvfrom(1024)
             message = data.decode()
@@ -31,8 +15,6 @@ def receive_messages(node_id, ip, port):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a node in the distributed system.')
     parser.add_argument('--node_id', type=str, required=True, help='Unique identifier for the node.')
-    parser.add_argument('--ip', type=str, default='127.0.0.1', help='IP address for the node.')
-    parser.add_argument('--port', type=int, default=12345, help='Port number for the node.')
     args = parser.parse_args()
 
-    receive_messages(args.node_id, args.ip, args.port)
+    receive_messages(args.node_id)
